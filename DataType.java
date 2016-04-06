c16e5bcc854fa5250be18de949c791519015f4ad
Sang Nguyen <shn2020@email.vccs.edu>
2016-04-06 14:55:01 -0400

    first commit

---
+++ DataType.java
@@ -0,0 +1,12 @@
+public class DataType {
+	public static void main(String[] args) {
+		System.out.println("1 / 2 = " + 1/2 + ", int type ");
+		System.out.println("1 % 2 = " + 1%2 + ", int type ");
+		System.out.println("1.0 / 2 = " + 1.0/2 + ", double type ");
+		System.out.println("5 + 7 / 2 = " + (5 + 7 / 2) + ", int type ");
+		System.out.println("\"Beat\" + " + "' '" + " + \"Army\" = " + "Beat Army" + ", String type "  );
+		System.out.println("6 + 13 / 5 - 35 % 3 = " +(6 + 13 / 5 - 35 % 3)+ ", int type " );
+		System.out.println("3.5 * (5/4) = " + (3.5 * (5/4)) + ", double type");
+		System.out.println("(3.5 * 5)/4 = " + ((3.5 * 5)/4) + ", double type");
+	}
+}
---
+++ Demo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Demo {
+	public static void main(String[] args) {
+		int[] num = new int[5];
+		Scanner input = new Scanner(System.in);
+		int sum = 0;
+		int max = -10000000;
+		int min = 10000000;
+		int median = 0;
+		double average = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Enter value for number " + (i+1) + ": ");
+			num[i] = input.nextInt();
+			sum += num[i];
+			if (num[i] > max) max = num[i];
+			if (num[i] < min) min = num[i];
+		}
+		median = num[3];
+		average = sum/5;
+		
+		System.out.println("Sum = " + sum);
+		System.out.println("Average = " + average);
+		System.out.println("Max = " + max);
+		System.out.println("Min = " + min);
+		System.out.println("Median = " + median);
+		
+	}
+}
